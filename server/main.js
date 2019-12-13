4d54e6f9e492e8f2c9288ee0bbff01345c7ad9a5
John Bonde <jjbonde1@gmail.com>
2019-12-13 10:21:45 -0700

    main get working

--- server/main.js
+++ server/main.js
@@ -35,10 +35,10 @@ server.use(bp.json());
 //NOTE next we want to register all our routes(doorways that can be accessed in our app)
 
 //NOTE we have to import access to our controllers
-import ValuesController from "./controllers/ValuesController";
+import PostsController from "./controllers/PostsController";
 
 //NOTE remember the forward slash at the start of your path!
-server.use("/api/values", new ValuesController().router);
+server.use("/api/posts", new PostsController().router);
 
 //NOTE Everything below this line always stays the same
 
